b4fb41fc48d22abf37469451c591cef7f78de4de
Sibuuu07 <95744973+Sibuuu07@users.noreply.github.com>
2022-04-02 16:06:55 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -22,14 +22,14 @@ global.APIKeys = {
 }
 
 //═══════[modification]════════\\
-global.owner = ['916909137213','6283839980451'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
-global.pemilik = ['916909137213'] //another owner number
-global.premium = ['916909137213'] //premium number
-global.pengguna = 'Xeon' //username
-global.botnma = 'Cheems Bot-MD' //bot name
-global.ownernma = '🦄Dream Guy Xeon' //owner name
-global.packname = 'Cheems Bot' //sticker package name
-global.author = 'By Dream Guy Xeon' //sticker author name
+global.owner = ['919337956084','16149454333'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
+global.pemilik = ['919337956084'] //another owner number
+global.premium = ['919337956084'] //premium number
+global.pengguna = 'Arctix' //username
+global.botnma = 'Arctix' //bot name
+global.ownernma = 'Arctix' //owner name
+global.packname = 'Arctix' //sticker package name
+global.author = 'By Arctix' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '⭔' //design
